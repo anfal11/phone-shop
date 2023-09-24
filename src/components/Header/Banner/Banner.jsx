ff9b439f2e872809f2181a20369f4a2e37e61867
anfal11 <anfal15-14303@diu.edu.bd>
2023-09-25 05:41:12 +0600

    banner responsive done

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => {
     return (
-        <div className="h-[70vh]">
+        <div className="lg:h-[70vh]">
             <img className="w-full" src="https://png.pngtree.com/thumb_back/fw800/back_our/20190621/ourmid/pngtree-cool-new-mobile-phone-promotion-purple-banner-image_183067.jpg" alt="" />
         </div>
     );
